0078bae08509615cb6a58e169d6164fdba1010d6
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-12-13 15:17:34 +0200

    Update main.tf

--- random_pet/main.tf
+++ random_pet/main.tf
@@ -1,7 +1,8 @@
 resource "random_pet" "name" {
-length = "4"
-separator = "-"
+  length    = "4"
+  separator = "-"
 }
+
 output "pet_module" {
-value = "${random_pet.name.id}"
+  value = "${random_pet.name.id}"
 }
